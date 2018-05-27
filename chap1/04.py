b7b7e18e6e1b1589a0cbff07575fb771c19c4409
ravrik <conver3510@gmail.com>
2018-05-27 15:24:19 +0900

    c1q4 repair1

--- chap1/04.py
+++ chap1/04.py
@@ -1,10 +1,12 @@
 s = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 s = s.split()
 key=[]
+value=[]
 for index, word in enumerate(s):
     if index+1 in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
         key.append(word[0])
     else:
         key.append(word[:2])
-dic = dict(zip(key, s))
+    value.append(index)
+dic = dict(zip(key, value))
 print(dic)
